e270f62e8af2eb4e5adc63e0ef0dd2af670fd968
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2017-08-08 16:01:36 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+printf("possitive");
+elseif(n<0)
+printf("negative");
+else
+printf("zero");
+}
