193ce0f0c812b40a53400c0c6ad2b572613e2c2f
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2017-09-05 16:02:23 +0530

    Create consonant.c

---
+++ consonant.c
@@ -0,0 +1,14 @@
+	scanf("%c",n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='u')
+	{
+		printf("the given character is a vowel");
+	}
+	else
+	{
+		printf("given character is a consonant");
+	}
+	
+	return 0;
+
+}
+#include <stdio.h>
