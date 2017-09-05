71cf228ec987461e49a24c15e4ccd11f6098fdd1
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2017-09-05 15:50:22 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+	
+		if(n%2==0)
+		{
+	
+	printf("the number is even");
+		}
+		else
+		{
+			printf("num is odd");
+		}
+	return 0;
+}
