6d21c2d4a3ddbe9a49d623459fce8c11075ad285
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2017-09-13 18:54:28 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+int n;
+printf("enter the number between 1 to 9");
+scanf("%d",&n);
+if(n>=1&&n<=9)
+{
+printf("the number is between the range");
+}
+else
+printf("the number is not between the range");
+
+	return 0;
+}
+
