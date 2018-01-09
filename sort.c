d172df5ce5c1b5899ca6fce0715b89eb4a920acc
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-09 16:23:10 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    int num,a[1000],i,t,j;
+    printf("enter total numbers");
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<num;i++)
+    {
+        for(j=1;j<num;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[j];
+                a[j]=a[i];
+                a[i]=t;
+            }
+        }
+    }
+    for(i=0;i<num;i++)
+    printf("%d\t",a[i]);
+
+
+	return 0;
+}
