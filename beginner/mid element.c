762a01a6503e6b87497e2311ff02d325dd4da89d
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-18 18:04:43 +0530

    Create mid element.c

---
+++ beginner/mid element.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[1000],i,j,t,limit;
+	scanf("%d",&limit);
+	for(i=0;i<limit;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<limit;i++)
+	{
+		for(j=0;j<limit;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[j];
+				a[j]=a[i];
+				a[i]=t;
+			 }
+			
+		}
+	}
+	printf("%d\t",a[limit%2]);
+	return 0;
+}
