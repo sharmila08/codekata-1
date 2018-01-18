fac71f7998a27363082017373d1c4d781446002f
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-18 16:19:33 +0530

    Create pallindrome.c

---
+++ beginner/pallindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,value,k,r,ans=0;
+	printf("enter the number");
+	scanf("%d",&value);
+	k=value;
+	while(k>0)
+	{
+		r=k%10;
+		ans=(ans*10)+r;
+		k=k/10;
+	}
+	if(ans==value)
+	printf("yes");
+	else
+	printf("no");
+	return 0;
+}
