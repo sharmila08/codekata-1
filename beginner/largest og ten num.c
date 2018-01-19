8b8ae997e1a7395e6c1281c30324993fea3eefc4
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-19 22:33:06 +0530

    Update largest og ten num.c

--- beginner/largest og ten num.c
+++ beginner/largest og ten num.c
@@ -2,12 +2,11 @@
 
 int main(void) {
 	int a[1000],i,j,t,limit;
-	scanf("%d",&limit);
-	for(i=0;i<limit;i++)
+	for(i=0;i<10;i++)
 	scanf("%d",&a[i]);
-	for(i=0;i<limit;i++)
+	for(i=0;i<10;i++)
 	{
-		for(j=0;j<limit;j++)
+		for(j=0;j<10;j++)
 		{
 			if(a[i]>a[j])
 			{
