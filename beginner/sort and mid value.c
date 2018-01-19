835502fca42d9548df72ac44a87bb2fa4141084a
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-19 13:37:04 +0530

    Update sort and mid value.c

--- beginner/sort and mid value.c
+++ beginner/sort and mid value.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[1000],i,j,t,limit;
-	scanf("%d",&limit);
-	for(i=0;i<limit;i++)
+	int a[1000],i,j,t,lim;
+	scanf("%d",&lim);
+	for(i=0;i<lim;i++)
 	scanf("%d",&a[i]);
 	for(i=0;i<limit;i++)
 	{
-		for(j=0;j<limit;j++)
+		for(j=0;j<limt;j++)
 		{
 			if(a[i]>a[j])
 			{
