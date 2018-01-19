c1cda2a37a9b062cddb5d0e77c93d82574d2f266
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-01-19 14:22:41 +0530

    Update swap using biteise opp.c

--- beginner/swap using biteise opp.c
+++ beginner/swap using biteise opp.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main() {
-    int i = 65;
-    int k = 120;
-    printf(" value of i=%d k=%d before swapping", i, k);
-
+    int i ;
+    int k;
+    scanf("%d%d",&i,&k);
     i = i ^ k;
     k = i ^ k;
     i = i ^ k;
-    printf("value of i=%d k=%d after swapping", i, k);
+    printf("%d %d", i, k);
 
     return 0;
 }
